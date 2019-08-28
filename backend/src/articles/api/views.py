b11a285cfd0bae0bab5380add9799f996b11c39b
LugovoiD <51412904+LugovoiD@users.noreply.github.com>
2019-08-28 20:33:52 +0200

    Add files via upload

---
+++ backend/src/articles/api/views.py
@@ -0,0 +1,8 @@
+from articles.models import Article
+from .serializers import ArticleSerializer
+from rest_framework import viewsets
+
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
